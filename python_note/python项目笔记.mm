07657976bc79955a3fd977003057b1ced0a0a63c
llm <3282276170@qq.com>
2015-10-12 15:32:26 +0800

    update project readme

---
+++ python_note/python项目笔记.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1444555555500" ID="ID_1669436139" MODIFIED="1444555572440" TEXT="python &#x9879;&#x76ee;&#x7b14;&#x8bb0;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+</node>
+</map>
