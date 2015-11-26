b3472d5ac5f4e4cbcc5bc022b72db47b5ff1be1b
llm <3282276170@qq.com>
2015-11-26 18:54:59 +0800

    Add commit

--- Beginning-Python/10/10.3.7.py
+++ Beginning-Python/10/10.3.7.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
+# 10.3.7
+# shelve.open函数返回的对象不是普通的映射
+import shelve
+s = shelve.open('test.dat')
+s['x'] = ['a', 'b', 'c']
+s['x'].append('d')
+print s['x']
+# ['a', 'b', 'c']
+
+import shelve
+s = shelve.open('test.dat')
+s['x'] = ['a', 'b', 'c']
+temp = s['x']
+temp.append('d')
+s['x'] = temp
+print s['x']
+# ['a', 'b', 'c', 'd']
+
+
 
